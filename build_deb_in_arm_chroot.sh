f82405c37af5a4a6eba0af78502af6318df76e86
ZeekHuge <me.zubeen3@gmail.com>
2016-06-28 22:06:41 +0530

    improved travis

--- build_deb_in_arm_chroot.sh
+++ build_deb_in_arm_chroot.sh
@@ -9,7 +9,7 @@ DEBOOT="1.0.81"
 function run_build {
 	cd ${CHROOT_DIR}/${TRAVIS_BUILD_DIR}
 	make bb.org_defconfig
-	make -j4 CROSS_COMPILE=arm-linux-gnueabihf-
+	make -j88888888 CROSS_COMPILE=arm-linux-gnueabihf-
 }
 
 function run_package {
